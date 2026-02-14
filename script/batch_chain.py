8625c7b8546ff1ad9b2020b9403a6e17bd761179
tylerlumsden <turtletyler03@gmail.com>
2026-02-14 18:31:40 -0500

    Fixed chaining logic with hacky variable unsetting

--- script/batch_chain.py
+++ script/batch_chain.py
@@ -6,21 +6,33 @@ def chain_jobs(jobs: list[Slurm]) -> Slurm:
     for i in range(len(rev) - 1):
         a, b = rev[i], rev[i + 1]
         b.add_cmd(
-            f"if [[ {Slurm.SLURM_ARRAY_TASK_ID} -eq {Slurm.SLURM_ARRAY_TASK_MIN} ]]; then\n"
-            f"  sbatch --dependency=afterok:$SLURM_JOB_ID << 'EOF-{i}'\n"
-            f"{a.script()}EOF-{i}\n"
+            f"if [ {Slurm.SLURM_ARRAY_TASK_ID} -eq {Slurm.SLURM_ARRAY_TASK_MIN} ]; then\n"
+            f"echo count: $SLURM_ARRAY_TASK_COUNT\n"
+            f"echo array id: $SLURM_ARRAY_TASK_ID\n"
+            f"echo array task min: $SLURM_ARRAY_TASK_MIN\n"
+            f"echo array job id: $SLURM_ARRAY_JOB_ID\n"
+            f"echo job id: $SLURM_JOB_ID\n"
+            f"JOB_ID=${{{Slurm.SLURM_ARRAY_JOB_ID[1:]}:-{Slurm.SLURM_JOB_ID}}}\n"
+            f"unset {Slurm.SLURM_ARRAY_JOB_ID[1:]}\n"
+            f"sbatch --dependency=afterok:$JOB_ID << 'EOF-{i}'\n"
+            f"{a.script(convert=False)}EOF-{i}\n"
             f"fi"
         )
     return jobs[0]
 
 jobs = []
-for i in range(24, 25):
+for i in range(24, 30):
     uncompress = Slurm(account="def-cbright", array=range(0, 100), job_name=f"uncompress99-{i}", time=datetime.timedelta(days=7))
-    uncompress.add_cmd(f"./build/debug/main 99 -c 3 1 --uncompress --paf=-2 --line={i} -n $SLURM_ARRAY_TASK_COUNT -j $SLURM_ARRAY_TASK_ID")
+    #uncompress.add_cmd(f"echo test-{i}")
+    uncompress.add_cmd(f"./build/debug/main 99 -c 3 1 --uncompress --paf=-2 --line={i} -n {Slurm.SLURM_ARRAY_TASK_COUNT} -j {Slurm.SLURM_ARRAY_TASK_ID}")
     jobs.append(uncompress)
 
     match = Slurm(account="def-cbright", job_name=f"match99-{i}", time=datetime.timedelta(days=1), mem="5G")
-    match.add_cmd("./build/debug/main 99 -c 1 --paf=-2 -n 100 --sort --match")
+    match.add_cmd("./build/debug/main 99 -c 1 --paf=-2 -n 100 --sort --match --append")
+    #match.add_cmd(f"echo testmatch-{i}")
     jobs.append(match)
 
-print(chain_jobs(jobs))
+job = chain_jobs(jobs)
+
+print(job.script())
+job.sbatch()
--- script/python_load.sh
+++ script/python_load.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 echo Swapping to virtual environment
 source ./golay/bin/activate
+unset SQUEUE_FORMAT
